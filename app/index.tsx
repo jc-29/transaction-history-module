bbac05d3817db0fc59efd29637bb2a248f867619
jc-29 <jonathanchng703@gmail.com>
2024-12-15 02:42:21 +0800

    Login Page
    Basic Login Page UI

---
+++ app/index.tsx
@@ -0,0 +1,61 @@
+import { View, Text, Alert, TouchableOpacity } from 'react-native';
+import { useState, useEffect } from 'react';
+import * as LocalAuthentication from 'expo-local-authentication';
+
+export default function Index() {
+  const [isBiometricsAvailable, setIsBiometricsAvailable] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const checkBiometricSupport = async () => {
+      const hardwareSupported = await LocalAuthentication.hasHardwareAsync();
+      const supportedTypes = await LocalAuthentication.supportedAuthenticationTypesAsync();
+
+      console.log('Hardware Supported:', hardwareSupported);
+      console.log('Supported Types:', supportedTypes);
+
+      if (
+        hardwareSupported
+      ) {
+        setIsBiometricsAvailable(true);
+      } else {
+        Alert.alert('Biometrics not available', 'Your device does not support biometric authentication.');
+      }
+    };
+
+    checkBiometricSupport();
+  }, []);
+
+  const handleBiometricLogin = async () => {
+    try {
+      const result = await LocalAuthentication.authenticateAsync({
+        promptMessage: 'Login with Biometrics',
+        fallbackLabel: 'Enter Password',
+        disableDeviceFallback: false,
+      });
+
+      if (result.success) {
+        setIsAuthenticated(true);
+        Alert.alert('Success', 'You are authenticated!');
+      } else {
+        Alert.alert('Error', 'Authentication failed or canceled.');
+      }
+    } catch (error) {
+      Alert.alert('Error', 'An unexpected error occurred.');
+      console.error(error);
+    }
+  };
+
+  return (
+    <View className="w-screen h-screen bg-black flex items-center justify-center">
+        <Text className='text-white mb-5 text-3xl font-semibold'>Welcome to Your Secure Transaction History App</Text>
+        <Text className='text-white mb-5'>Biometrics Login {!isBiometricsAvailable && 'Not'}Available</Text>
+      <TouchableOpacity
+        className="rounded-xl px-10 py-2 bg-white text-black"
+        onPress={handleBiometricLogin}
+      >
+        <Text className="text-2xl font-bold">Login</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
